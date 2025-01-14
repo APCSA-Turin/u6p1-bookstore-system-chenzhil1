a192d1b4729071338d291d24fc2b170ac8cbf783
Chen Zhi Lin <chenzhil@nycstudents.net>
2025-01-14 14:32:02 +0000

    a

--- src/main/java/com/example/project/BookStore.java
+++ src/main/java/com/example/project/BookStore.java
@@ -3,19 +3,41 @@ package com.example.project;
 public class BookStore{
 
     //requires at least 2 attributes Book[] books, User[] users (initialized to an empty array of 10 max users) 
-
+    private Book[] books;
+    private User[] users = new User[10];
     //requires 1 empty constructor
+    public BookStore() {}
 
     // public getUsers(){}
+    public User[] getUsers() {
+        return users;
+    }
 
     // public setUsers(){}
+    public void setUsers(User[] newUsers) {
+        users = newUsers;
+    }
 
     // public  getBooks(){}
+    public Book[] getBooks() {
+        return books;
+    }
 
     // public void addUser(User user){} 
-
+    public void addUser(User user) {
+        for(int i = 0; i < users.length; i ++) {
+            if(users[i] == null) {
+                users[i] = user;
+                break;
+            }
+        }
+    }
     // public void removeUser(User user){}
-
+    public void removeUser(User user) {
+        for(int i = 0; i < users.length; i ++) {
+            
+        }
+    }
     // public void consolidateUsers(){}
 
     // public void addBook(Book book){}
--- src/main/java/com/example/project/User.java
+++ src/main/java/com/example/project/User.java
@@ -2,23 +2,61 @@ package com.example.project;
 
 public class User{
     //requires 3 private attributes String name, String Id, Book book that is initialized to empty
-
+    private String name;
+    private String Id;
+    private Book[] books = new Book[5];
     //requires 1 contructor with two parameters that will initialize the name and id
- 
-    // public  getName() {}
+    public User(String name, String Id) {
+        this.name = name;
+        this.Id = Id;
+    }
 
-    // public  setName() {}
+    // public  getName() {}
+    public String getName() {
+        return name;
+    }
+    // public  setName()  {}
+    public void setName(String newName) {
+        name = newName;
+    }
 
     // public  getId() {}
+    public String getId() {
+        return Id;
+    }
 
     // public void setId() {}
+    public void setId(String newID) {
+        Id = newID;
+    }
 
     // public getBooks() {}
+    public Book[] getBook() {
+        return books;
+    }
 
     // public setBooks() {}
+    public void setBooks(Book[] newBooks) {
+        books = newBooks;
+    }
 
     // public String bookListInfo(){} //returns a booklist for the user, if empty, output "empty"
-
+    public String bookListInfo() {
+        String bookList = "";
+        for(Book book: books) {
+            if(book != null) {
+                bookList += book.bookInfo() + "\n";
+            }
+            else {
+                bookList += "empty\n";
+            }
+        }
+        return bookList;
+        
+    }
     // public String userInfo(){} //returns  "Name: []\nID: []\nBooks:\n[]"
-       
+    public String userInfo() {
+        String info = "Name: " + name + "\nID: " + Id + "\nBooks:\n" + bookListInfo();
+        return info;
+    }
 }
